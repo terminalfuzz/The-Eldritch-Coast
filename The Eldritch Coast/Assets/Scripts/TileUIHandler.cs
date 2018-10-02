86eb35b4b631d78d450cd7e558070d87e6e8877e
fuzz <terminal.fuzz@gmail.com>
2018-10-02 18:21:17 -0400

    Uploading the actual game project

---
+++ The Eldritch Coast/Assets/Scripts/TileUIHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileUIHandler : MonoBehaviour {
+
+    public int tileX;
+    public int tileY;
+
+    private MeshRenderer meshRenderer;
+    private bool isSelected;
+    private Color myColor;
+
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+        myColor = meshRenderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        meshRenderer.material.color = Color.red;
+        Debug.Log("Current Position: (" + tileX + ", " + tileY + ")");
+        GameManager.Instance.findBestRouteTo(tileX, tileY);
+    }
+
+    private void OnMouseExit()
+    {
+        meshRenderer.material.color = myColor;        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked: (" + tileX + ", " + tileY + ")");
+        
+    }
+
+}
---
+++ The Eldritch Coast/Assets/Scripts/UserPlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPlayerScript : PlayerScript {
+
+    public int tileX;
+    public int tileY;
+
+    public List<Node> currentPath = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentPath != null)
+        {
+            Debug.Log("Player path is no longer null");
+            int currNode = 0;
+            while (currNode < currentPath.Count - 1)
+            {
+                Vector3 start = GameManager.Instance.TileCoordToWorldCoord(currentPath[currNode].x, currentPath[currNode].y) + new Vector3(0, 0.3f, 0);
+                Vector3 end = GameManager.Instance.TileCoordToWorldCoord(currentPath[currNode + 1].x, currentPath[currNode + 1].y) + new Vector3(0, 0.3f, 0);
+
+                Debug.DrawLine(start, end, Color.red);
+
+                currNode++;
+            }
+        }
+	}
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Player Clicked, Also: " + currentPath.Count);
+
+    }
+
+}
